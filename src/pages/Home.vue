2609e270c3f514252707a77538fcffa48a976e4d
Tanis Webb <taniswebb@gmail.com>
2019-05-16 00:28:43 +0200

    made h3 and span translatable; removed stuff;

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -5,8 +5,8 @@
     class="my-5"
     align-center
   >
-    <h3>Welcome to Orbit's Vue.js Boilerplate</h3>
-    <span>Use this, or don't. I don't care :)</span>
+    <h3>{{ $t('home.headline') }}</h3>
+    <span>{{ $t('home.subheader') }}</span>
   </v-layout>
 </template>
 
@@ -15,37 +15,7 @@ export default {
   name: 'Home',
   data () {
     return {
-      // Three Important Main Site
-      main3: {
-        items: {
-          one: {
-            id: 1,
-            icon: 'fa-truck',
-            title: 'home.main3.first.title',
-            text: 'home.main3.first.text'
-          },
-          two: {
-            id: 2,
-            icon: 'fa-truck',
-            title: 'home.main3.second.title',
-            text: 'home.main3.second.text'
-          },
-          three: {
-            id: 3,
-            icon: 'fa-truck',
-            title: 'home.main3.third.title',
-            text: 'home.main3.third.text'
-          }
-        }
-      }
-    }
-  },
-  computed: {
-    options () {
-      return {
-        // for scrolling, might be useless
-        offset: 100
-      }
+      // data here and stuffs
     }
   }
 }
