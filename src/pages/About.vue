e41c53501971726a5af3256c20c11952e6e18f97
Tanis Webb <taniswebb@gmail.com>
2019-05-16 00:41:11 +0200

    Added About.vue

---
+++ src/pages/About.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-layout
+    column
+    wrap
+    class="my-5"
+    align-center
+  >
+    <h3>{{ $t('about.headline') }}</h3>
+    <span>{{ $t('about.subheader') }}</span>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      // data here and stuffs
+    }
+  }
+}
+</script>
+
+<style>
+</style>
