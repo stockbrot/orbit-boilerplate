8f1d12b71cf2836fffccd2407c8db65c8f9c7d9b
Tanis Webb <taniswebb@gmail.com>
2019-05-27 14:22:41 +0200

    Added layout

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-content>
+    <ToolbarComp/>
+
+    <v-container fluid fill-height>
+      <router-view class="routerclass"/>
+    </v-container>
+  </v-content>
+</template>
+
+<script>
+import ToolbarComp from '@/components/ToolbarComp'
+
+export default {
+  name: 'MainLayout',
+  components: {
+    ToolbarComp
+  },
+  data () {
+    return {
+    }
+  },
+}
+</script>
+
+<style lang="stylus">
+.routerclass
+  padding-top: 50px
+
+</style>
