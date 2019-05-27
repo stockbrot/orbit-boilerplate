ed5a33c66196b393d27570046cdf27d8252df652
Tanis Webb <taniswebb@gmail.com>
2019-05-27 14:22:05 +0200

    removed old Navigation

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,16 @@
 <template>
   <v-app dark id="app">
-    <Navigation/>
     <router-view/>
     <Footer/>
   </v-app>
 </template>
 
 <script>
-import Navigation from './components/Navigation'
 import Footer from './components/Footer'
 
 export default {
   name: 'App',
   components: {
-    Navigation,
     Footer
   }
 }
